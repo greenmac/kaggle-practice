1886c7a6e7307335a0939577f2554eaa26ee7de1
greenmac <alwaysmac@msn.com>
2019-03-31 15:30:31 +0800

    add fillna

--- titanic/my_first_kaggle.py
+++ titanic/my_first_kaggle.py
@@ -1,17 +1,34 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath( __file__ ))
-test_data = pd.read_csv(SCRIPT_PATH + "/test.csv")
+dataset = pd.read_csv(SCRIPT_PATH + "/train.csv")
 
-#產生一個長度跟test data一樣的序列，50%的機率是0，50%的機率是1
-rand_labels = (np.random.rand(len(test_data['PassengerId'])) > 0.5).astype(np.int32)
+#features = ['Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin', 'Embarked']
+training_data = dataset[features]
+training_label = dataset['Survived']
 
-results = pd.DataFrame({
-    'PassengerId' : test_data['PassengerId'],
-    'Survived' : rand_labels
-})
+raining_data = training_data.fillna(0)
+# print(raining_data)
+#training_data['Name'] = LabelEncoder().fit_transform(training_data['Name']) # 不重要
+training_data['Sex'] = LabelEncoder().fit_transform(training_data['Sex'])
+#training_data['Ticket'] = LabelEncoder().fit_transform(training_data['Ticket']) # 不重要
+# training_data['Cabin'] = LabelEncoder().fit_transform(training_data['Cabin'].astype(str))
+# training_data['Embarked'] = LabelEncoder().fit_transform(training_data['Embarked'].astype(str))
 
-results.to_csv(SCRIPT_PATH + "/submission1.csv", index=False)
-# print(rand_labels)
+# model = RandomForestClassifier()
+# model.fit(training_data, training_label)
+
+# print(training_data['Embarked'])
+
+# y_pos = np.arange(len(features))
+# plt.barh(y_pos, model.feature_importances_, align='center', alpha=0.4)
+# plt.yticks(y_pos, features)
+# plt.xlabel('features')
+# plt.title('feature_importances')
+# plt.show()
